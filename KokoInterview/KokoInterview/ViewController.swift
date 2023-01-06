40bef36a2bc8b3d380c0306fcf5431f4a28f2a57
Ying Hsun Chen <stoola20.526@gmail.com>
2023-01-06 12:49:55 +0800

    init proj

---
+++ KokoInterview/KokoInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KokoInterview
+//
+//  Created by Hsun Chen on 2023/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
